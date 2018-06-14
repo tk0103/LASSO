3c71f49902ab63e5546ba20e93777ab20b9a352f
tk0103 <ninntenntarou@gmail.com>
2018-06-14 14:12:32 +0900

    init

--- lasso.R
+++ lasso.R
@@ -1,5 +1,5 @@
 library(glmnet)
-ori = read.csv("C:\\Users\\Yuri\\Desktop\\lasso30.csv",header = T,colClasses = "numeric")
+ori = read.csv("lasso30.csv",header = T,colClasses = "numeric")
 test <- matrix(ori$ex1)
 dic <- cbind(ori$ex2,ori$ex3,ori$ex4,ori$ex5,ori$ex6,ori$ex7,ori$ex8,ori$ex9,ori$ex10,ori$ex21,ori$ex22,ori$ex23,ori$ex24,ori$ex25,ori$ex26,ori$ex27,ori$ex28,ori$ex29,ori$ex30,all=T)
 fit <-glmnet(x = dic,y=test,alpha=1)
